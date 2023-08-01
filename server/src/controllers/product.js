f5021f1a14c80ea670be370a1b3241d088009593
nguyen <thai>
2023-08-01 21:24:04 +0700

    done ratings filter sort pagination fields product

--- server/src/controllers/product.js
+++ server/src/controllers/product.js
@@ -23,10 +23,41 @@ const getProduct = asyncHandler(async(req, res) => {
 })
 
 const getProducts = asyncHandler(async(req, res) => {
-    const products = await Product.find()
-    return res.status(200).json({
-        success: products ? true : false,
-        productsData: products ? products : 'Cannot get products'
+    const queries = { ...req.query }
+
+    const excludeFields = ['limit', 'sort', 'page', 'fields']
+    excludeFields.forEach(el => delete queries[el])
+
+    let queryString = JSON.stringify(queries)
+    queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`) 
+    const formatedQueries = JSON.parse(queryString)
+
+    if (queries?.title) formatedQueries.title = {$regex: queries.title, $options: 'i'}
+    let queryCommand = Product.find(formatedQueries)
+
+    if(req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ')
+        queryCommand = queryCommand.sort(sortBy)
+    }
+
+    if (req.query.fields) {
+        const fields = req.query.fields.split(',').join(' ')
+        queryCommand = queryCommand.select(fields)
+    }
+
+    const page = +req.query.page || 1
+    const limit = +req.query.limit || process.env.LIMIT_PRODUCTS
+    const skip = ( page - 1 ) * limit
+    queryCommand.skip(skip).limit(limit)
+
+    queryCommand.exec(async(err, response) => {
+        if (err) throw new Error(err.message)
+        const counts = await Product.find(formatedQueries).countDocuments()
+        return res.status(200).json({
+            success: response ? true : false,
+            counts,
+            products: response ? response : 'Cannot get product',
+        })
     })
 })
 
@@ -34,7 +65,6 @@ const updateProduct = asyncHandler(async(req, res) => {
     const { pid } = req.params
     if ( !pid || Object.keys(req.body).length === 0) throw new Error("Missing input")
     if ( req.body && req.body.title ) req.body.slug = slugify(req.body.title)
-    console.log(req.body)
     const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, { new: true })
     return res.status(200).json({
         success: updatedProduct ? true : false,
@@ -51,10 +81,46 @@ const deleteProduct = asyncHandler(async(req, res) => {
     })
 })
 
+const ratings = asyncHandler(async(req, res) => {
+    const { _id } = req.user
+    const { star, comment, pid } = req.body
+    if (!star || !pid) throw new Error('Missing input')
+    const ratingProduct = await Product.findById(pid)
+    const alreadyRating = ratingProduct.ratings.find(el => el.postedBt.toString() === _id)
+    if (alreadyRating) {
+        // update rating
+        await Product.updateOne({
+            ratings: { $elemMatch: alreadyRating }
+        }, {
+            $set: { "ratings.$.star": star, "ratings.$.comment": comment }
+        }, { new: true })
+
+    } else {
+        // add rating
+        await Product.findByIdAndUpdate(pid, {
+            $push: { ratings: {star, comment, postedBt: _id} }
+        }, { new: true })
+    }
+
+    // Sum ratings
+    const updatedProduct = await Product.findById(pid)
+    const ratingCount = updatedProduct.ratings.length
+    const sumRatings = updatedProduct.ratings.reduce((sum, el) => sum + el.star, 0  )
+    updatedProduct.totalRatings = Math.round(sumRatings / ratingCount)
+
+    await updatedProduct.save()
+
+    return res.status(200).json({
+        success: true,
+        updatedProduct
+    })
+})
+
 module.exports = {
     createProduct,
     getProduct,
     getProducts,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    ratings
 }
--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -4,6 +4,7 @@ const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
 
 router.post('/', [verifyAccessToken, isAdmin], ctrls.createProduct)      
+router.put('/ratings', [verifyAccessToken], ctrls.ratings)      
 router.get('/:uid', ctrls.getProduct)      
 router.get('/', ctrls.getProducts)    
 router.put('/:pid', [verifyAccessToken, isAdmin], ctrls.updateProduct)  
